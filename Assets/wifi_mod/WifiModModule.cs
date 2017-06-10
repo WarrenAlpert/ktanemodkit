d736609d85261b61620e6e3ceb85480b8c6be13d
Warren Alpert <WarrenAlpert@outlook.com>
2017-06-10 14:04:16 -0700

    Add test sprite and unique web server ports

--- Assets/wifi_mod/WifiModModule.cs
+++ Assets/wifi_mod/WifiModModule.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Net;
+using System.Threading;
+using System;
 
 public class WifiModModule : MonoBehaviour
 {
+    public static List<int> usedPorts = new List<int>();
     public KMSelectable[] buttons;
     KMAudio.KMAudioRef audioRef;
     int correctIndex;
@@ -15,7 +19,7 @@ public class WifiModModule : MonoBehaviour
 
     void Init()
     {
-        correctIndex = Random.Range(0, 4);
+        correctIndex = UnityEngine.Random.Range(0, 4);
         GetComponent<KMBombModule>().OnActivate += OnActivate;
         GetComponent<KMSelectable>().OnCancel += OnCancel;
         GetComponent<KMSelectable>().OnLeft += OnLeft;
@@ -114,6 +118,191 @@ public class WifiModModule : MonoBehaviour
         if(audioRef != null && audioRef.StopSound != null)
         {
             audioRef.StopSound();
+        }
+    }
+
+    KMBombInfo bombInfo;
+    KMGameCommands gameCommands;
+    string modules;
+    string solvableModules;
+    string solvedModules;
+    string bombState;
+
+    Thread workerThread;
+
+    Queue<Action> actions;
+
+    void Awake()
+    {
+        actions = new Queue<Action>();
+        bombInfo = GetComponent<KMBombInfo>();
+        bombInfo.OnBombExploded += OnBombExplodes;
+        bombInfo.OnBombSolved += OnBombDefused;
+        gameCommands = GetComponent<KMGameCommands>();
+        List<string> ss = bombInfo.GetModuleNames();
+        Debug.Log(string.Join(",", ss.ToArray()));
+        bombState = "NA";
+        // Create the thread object. This does not start the thread.
+        Worker workerObject = new Worker(this);
+        workerThread = new Thread(workerObject.DoWork);
+        // Start the worker thread.
+        workerThread.Start(this);
+    }
+
+    void Update()
+    {
+        if (actions.Count > 0)
+        {
+            Action action = actions.Dequeue();
+            action();
+        }
+    }
+
+    void OnDestroy()
+    {
+        workerThread.Abort();
+    }
+
+    // This example requires the System and System.Net namespaces.
+    public void SimpleListenerExample(string[] prefixes)
+    {
+        // Create a listener.
+        HttpListener listener = new HttpListener();
+        // Add the prefixes.
+        foreach (string s in prefixes)
+        {
+            listener.Prefixes.Add(s);
+        }
+        listener.Start();
+        while (true)
+        {
+            // Note: The GetContext method blocks while waiting for a request. 
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+            // Construct a response.
+            string responseString = "";
+
+            if (request.Url.OriginalString.Contains("bombInfo"))
+            {
+                responseString = GetBombInfo();
+            }
+
+            if (request.Url.OriginalString.Contains("startMission"))
+            {
+                string missionId = request.QueryString.Get("missionId");
+                string seed = request.QueryString.Get("seed");
+                responseString = StartMission(missionId, seed);
+            }
+
+            if (request.Url.OriginalString.Contains("causeStrike"))
+            {
+                string reason = request.QueryString.Get("reason");
+                responseString = CauseStrike(reason);
+            }
+
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+        }
+    }
+
+    protected string StartMission(string missionId, string seed)
+    {
+        actions.Enqueue(delegate () { gameCommands.StartMission(missionId, seed); });
+
+        return missionId + " " + seed;
+    }
+
+    protected string CauseStrike(string reason)
+    {
+        actions.Enqueue(delegate () { gameCommands.CauseStrike(reason); });
+
+        return reason;
+    }
+
+    protected string GetBombInfo()
+    {
+        if (bombInfo.IsBombPresent())
+        {
+            if (bombState == "NA")
+            {
+                bombState = "Active";
+            }
+        }
+        else if (bombState == "Active")
+        {
+            bombState = "NA";
+        }
+
+        string time = bombInfo.GetFormattedTime();
+        int strikes = bombInfo.GetStrikes();
+        modules = GetListAsHTML(bombInfo.GetModuleNames());
+        solvableModules = GetListAsHTML(bombInfo.GetSolvableModuleNames());
+        solvedModules = GetListAsHTML(bombInfo.GetSolvedModuleNames());
+
+        string responseString = string.Format(
+            "<HTML><BODY>"
+            + "<span>Time: {0}</span><br>"
+            + "<span>Strikes: {1}</span><br>"
+            + "<span>Modules: {2}</span><br>"
+            + "<span>Solvable Modules: {3}</span><br>"
+            + "<span>Solved Modules: {4}</span><br>"
+            + "<span>State: {5}</span><br>"
+            + "</BODY></HTML>", time, strikes, modules, solvableModules, solvedModules, bombState);
+
+        return responseString;
+    }
+
+    protected void OnBombExplodes()
+    {
+        bombState = "Exploded";
+    }
+
+    protected void OnBombDefused()
+    {
+        bombState = "Defused";
+    }
+
+    protected string GetListAsHTML(List<string> list)
+    {
+        string listString = "";
+
+        foreach (string s in list)
+        {
+            listString += s + ", ";
+        }
+
+        return listString;
+    }
+
+    public class Worker
+    {
+        WifiModModule service;
+
+        public Worker(WifiModModule s)
+        {
+            service = s;
+        }
+
+        // This method will be called when the thread is started. 
+        public void DoWork()
+        {
+            int port;
+            do
+            {
+                port = UnityEngine.Random.Range(8050, 8099);
+            }
+            while (usedPorts.Contains(port));
+
+            usedPorts.Add(port);
+            Debug.Log("usedPorts adding: " + port + ", count: " + usedPorts.Count);
+            service.SimpleListenerExample(new string[] { "http://*:" + port + "/" });
         }
     }
 }
